4d671ea3fe1bfdb7992d8b3c8dc6282556e3171d
Glenn Pratt <glenn.pratt@acquia.com>
2014-12-07 12:32:10 -0600

    Initial test for Chunk.

--- src/message/chunk.rs
+++ src/message/chunk.rs
@@ -14,7 +14,7 @@ impl Chunk {
     pub fn from_packet(packet: &[u8]) -> IoResult<Chunk> {
         if packet.len() > 12 {
             Ok(Chunk {
-                id: packet[2..9].to_vec(),
+                id: packet[2..10].to_vec(),
                 sequence_number: packet[10],
                 sequence_count: packet[11],
                 payload: packet[12..].to_vec(),
@@ -37,6 +37,23 @@ mod test {
     #[test]
     fn construct_from_packet() {
         let json = r#"{message":"foo","host":"bar","_utf8":"✓"}"#;
-        let bar = json[0..1];
+        let id = "amessage"; // @todo is this hex in other impls?
+        let sequence_number = 1;
+        let sequence_count =  1;
+
+        let mut bytes: Vec<u8> = vec![0x1e, 0x0f];
+        bytes.push_all(id.as_bytes());
+        bytes.push(sequence_number);
+        bytes.push(sequence_count);
+        bytes.push_all(json.as_bytes());
+        let packet = bytes.as_slice();
+
+        let chunk = Chunk::from_packet(packet).unwrap();
+
+        assert_eq!(id.as_bytes(), chunk.id.as_slice());
+        assert_eq!(sequence_number, chunk.sequence_number);
+        assert_eq!(sequence_count, chunk.sequence_count);
+        assert_eq!(json.as_bytes(), chunk.payload.as_slice());
+        println!("{}", chunk.arrival.unwrap());
     }
 }
