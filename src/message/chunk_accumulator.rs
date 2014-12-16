021842cc40456962a27cea636c9647cfe80d8724
Glenn Pratt <glenn.pratt@acquia.com>
2014-12-16 03:00:12 -0600

    Minimal working ChunkAccumulator.

--- src/message/chunk_accumulator.rs
+++ src/message/chunk_accumulator.rs
@@ -26,10 +26,7 @@ impl ChunkAccumulator {
                 self.map.insert(id, set);
                 Ok(None)
             }
-            Ok(string) => {
-                self.map.remove(&id);
-                Ok(string)
-            },
+            Ok(string) => Ok(string),
             Err(e)     => Err(e)
         }
     }
@@ -51,14 +48,14 @@ impl ChunkSet {
     }
 
     pub fn accept(&mut self, chunk: Chunk) -> IoResult<Option<String>> {
-        let number = chunk.sequence_number.to_uint().unwrap();
-        let index = chunk.sequence_count.to_uint().unwrap() - 1;
+        let number = chunk.sequence_number.to_uint().unwrap() - 1;
+        // let index = chunk.sequence_count.to_uint().unwrap() - 1;
         // if  index != self.chunks.len() || index >= number {
         //     panic!("invalid - this shouldn't panic tho :)");
         // }
-        match self.chunks[index] {
+        match self.chunks[number] {
             None => {
-                self.chunks[index] = Some(chunk);
+                self.chunks[number] = Some(chunk);
                 self.rcv_count += 1;
                 self.complete_or_none()
             },
@@ -68,7 +65,10 @@ impl ChunkSet {
 
     fn complete_or_none(&self) -> IoResult<Option<String>> {
         if self.rcv_count == self.chunks.len() {
-            let complete_message = self.chunks[0].clone().unwrap().payload;
+            let mut complete_message = vec![];
+            for chunk in self.chunks.iter() {
+                complete_message.push_all(chunk.clone().unwrap().payload.as_slice());
+            }
             // let complete_message = self.chunks[0].unwrap().payload.clone();
             // Delete or allow cleanup thread to do that?
             // Generate complete message.
@@ -84,6 +84,7 @@ impl ChunkSet {
 mod test {
     use super::*;
     use std::collections::HashMap;
+    use std::rand::{OsRng, Rng};
     use message::Chunk;
 
     #[test]
@@ -106,17 +107,46 @@ mod test {
         let json = r#"{message":"foo","host":"bar","_utf8":"✓"}"#;
 
         let chunks = chunker(json, 22);
-        let packet = chunks[0].as_slice();
+        let packet1 = chunks[0].as_slice();
+        let packet2 = chunks[1].as_slice();
 
-        let chunk = Chunk::from_packet(packet).unwrap();
+        let chunk1 = Chunk::from_packet(packet1).unwrap();
+        let chunk2 = Chunk::from_packet(packet2).unwrap();
         let mut acc = ChunkAccumulator::new();
-        let result = acc.accept(chunk).unwrap().unwrap();
+        acc.accept(chunk1);
+        let result = acc.accept(chunk2).unwrap().unwrap();
         assert_eq!(json, result.as_slice());
     }
 
-    fn chunker(message: &str, max_length: uint) -> Vec<Vec<u8>> {
+    #[test]
+    fn two_chunked_messages() {
+        // Who knows if this should even be supported?
+        let json_a = r#"{message":"foo","host":"bar","_utf8":"✓"}"#;
 
-        let id = "amessage"; // @todo is this hex in other impls?
+        let chunks_a = chunker(json_a, 22);
+
+        let json_b = r#"{message":"foo","host":"bar","_utf8":"✓"}"#;
+
+        let chunks_b = chunker(json_b, 22);
+        let chunk_b_1 = Chunk::from_packet(chunks_b[0].as_slice()).unwrap();
+        let chunk_b_2 = Chunk::from_packet(chunks_b[1].as_slice()).unwrap();
+
+        let chunk_a_1 = Chunk::from_packet(chunks_a[0].as_slice()).unwrap();
+        let chunk_a_2 = Chunk::from_packet(chunks_a[1].as_slice()).unwrap();
+        let mut acc = ChunkAccumulator::new();
+        acc.accept(chunk_a_1);
+        acc.accept(chunk_b_1);
+        let result_a = acc.accept(chunk_a_2).unwrap().unwrap();
+        assert_eq!(json_a, result_a.as_slice());
+        let result_b = acc.accept(chunk_b_2).unwrap().unwrap();
+        assert_eq!(json_b, result_b.as_slice());
+    }
+
+    fn chunker(message: &str, max_length: uint) -> Vec<Vec<u8>> {
+        // Test only id.
+        let mut id = [0u8, .. 8];
+        let mut rng = OsRng::new().unwrap();
+        rng.fill_bytes(&mut id);
 
         let length = if message.len() > max_length {
             max_length
@@ -132,16 +162,12 @@ mod test {
         // Limit to max (255 for u8, GELF is lower, but meh).
         // Panics otherwise, should return Result(Err).
         let sequence_count = count.to_u8().unwrap();
-        // println!("{}", message.as_bytes().slice_to(length));
         let mut chunks = vec![];
         for x in range(0, sequence_count) {
-            println!("x     : {}", x);
             let sequence_number = (x + 1);
 
             let start = length * x.to_uint().unwrap();
             let end = length * (x + 1).to_uint().unwrap();
-            println!("start: {}", start);
-            println!("end  : {}", end);
             let part = if end <= length {
                 message.as_bytes()[start..end]
             } else {
@@ -149,18 +175,12 @@ mod test {
             };
 
             let mut bytes: Vec<u8> = vec![0x1e, 0x0f];
-            bytes.push_all(id.as_bytes());
+            bytes.push_all(id.as_slice());
             bytes.push(sequence_number);
             bytes.push(sequence_count);
             bytes.push_all(part);
-            println!("part: {}", part);
-            println!("part len: {}", part.len());
-            println!("bytes: {}", bytes);
-            println!("bytes len: {}", bytes.len());
             chunks.push(bytes);
         }
-        // for x in range()
-        println!("chunks {}", chunks);
         chunks
     }
 }
